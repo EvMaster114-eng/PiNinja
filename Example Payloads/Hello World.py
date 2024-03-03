e095a2a941682d4640a29c8b431fbb839d5f6c7c
EvMaster <73354669+EvMaster114-eng@users.noreply.github.com>
2024-03-03 14:59:55 -0800

    Update Hello World.py

--- Example Payloads/Hello World.py
+++ Example Payloads/Hello World.py
@@ -3,18 +3,19 @@
 #Author: EvMaster114
 #Description: Simple hello world script
 
+#Imports
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.keycode import Keycode as key
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from time import sleep
+#Varibales
+kbd = Keyboard(usb_hid.devices)
+lay = KeyboardLayoutUS(kbd)
+mse = Mouse(usb_hid.devices)
 
 def main():
-    kbd = Keyboard(usb_hid.devices)
-    lay = KeyboardLayoutUS(kbd)
-    mse = Mouse(usb_hid.devices)
-
     kbd.send(key.GUI, key.R)
     sleep(0.5)
     lay.write("notepad")
