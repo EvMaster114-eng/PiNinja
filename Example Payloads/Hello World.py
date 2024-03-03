4377d12e30b99a398359faed014362ebc4259ab7
EvMaster <73354669+EvMaster114-eng@users.noreply.github.com>
2024-03-03 09:00:54 -0800

    Create Hello World.py

---
+++ Example Payloads/Hello World.py
@@ -0,0 +1,30 @@
+#PiNinja Payload Script
+#Title: Hello World!
+#Author: EvMaster114
+#Description: Simple hello world script
+
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
+from adafruit_hid.keycode import Keycode as key
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from time import sleep
+
+def main():
+    kbd = Keyboard(usb_hid.devices)
+    lay = KeyboardLayoutUS(kbd)
+    mse = Mouse(usb_hid.devices)
+
+    kbd.send(key.GUI, key.R)
+    sleep(0.5)
+    lay.write("notepad")
+    sleep(0.5)
+    kbd.send(key.ENTER)
+    sleep(0.5)
+    kbd.send(key.LEFT_CONTROL, key.A)
+    sleep(0.5)
+    kbd.send(key.BACKSPACE)
+    sleep(0.5)
+    lay.write("Hello World!")
+
+
